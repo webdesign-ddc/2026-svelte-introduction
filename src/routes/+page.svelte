d3ebbb05df59e9e2569bbe06e7dd62c808f906fc
Tommaso Negri <me@tommasongr.com>
2026-04-03 12:29:39 +0200

    define and change $state
    $state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script>
-    console.log("hello world");
+    let count = $state(0)
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => count++ }>
+    clicks: {count}
+</button>
 
 <style>
     h1 {
