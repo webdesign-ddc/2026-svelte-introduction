123098fdc8e8600d291260f78180d7b410d7d839
Tommaso Negri <me@tommasongr.com>
2026-04-03 12:54:35 +0200

    add default value to $props
    $props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Counter initialCount={0} />
+<Counter />
 
 <style>
     h1 {
