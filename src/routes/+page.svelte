8fc13a56d1e568dca9e1d4264f911d961cd285d1
Tommaso Negri <me@tommasongr.com>
2026-04-03 12:59:47 +0200

    add state that needs to be $bindable
    $bindable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script>
     import Counter from "$lib/components/Counter.svelte";
+
+    let count = $state(5)
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Counter />
+<Counter initialCount={count} />
+
+<p>count from parent is {count}</p>
 
 <style>
     h1 {
