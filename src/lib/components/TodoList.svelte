35c1456387f49fad963a9b40d5813a9666c047a8
Tommaso Negri <me@tommasongr.com>
2026-04-03 18:18:16 +0200

    add example that needs #snippet
    #snippet

--- src/lib/components/TodoList.svelte
+++ src/lib/components/TodoList.svelte
@@ -4,6 +4,12 @@
         { title: "Assignment 2", done: true },
         { title: "Assignment 3", done: false }
     ])
+
+    let groceries = $state([
+        { title: "Apple",  done: false },
+        { title: "Bread",  done: false },
+        { title: "Salmon", done: false }
+    ])
 </script>
 
 <h3>Todo List</h3>
@@ -14,6 +20,19 @@
             <label>
                 <input type="checkbox" bind:checked={todo.done}>
                 {todo.title}
+            </label>
+        </li>
+    {/each}
+</ul>
+
+<h3>Groceries</h3>
+
+<ul>
+    {#each groceries as grocery}
+        <li>
+            <label>
+                <input type="checkbox" bind:checked={grocery.done}>
+                {grocery.title}
             </label>
         </li>
     {/each}
